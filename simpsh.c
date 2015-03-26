266d73abac11a559de05af31992a5d5b24a4915d
nathan gibson <gibson_nathaniel1@columbusstate.edu>
2015-03-26 15:59:18 -0400

    initial

---
+++ simpsh.c
@@ -0,0 +1,80 @@
+/* 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#define MAX_LENGTH 1024
+int main(int argc, char *argv[]) {
+	int interactive;
+	char line[MAX_LENGTH];
+	char* tokens[MAX_LENGTH];
+	char* token;
+	int number_of_tokens;
+	if(argc == 2){
+		if(strcmp("-i", argv[1]) == 0){
+			interactive = 1;
+		}else{
+			fprintf(stderr, "Incorrect command line arguments\n");
+			exit(-1);
+		}
+	}else{
+		interactive = 0;
+	}
+	while(1){
+		int i = 0;
+		if(interactive == 1)
+			printf("%% ");
+		if(fgets(line, MAX_LENGTH, stdin) == NULL)
+			return 0;
+		token = strtok(line, " ");
+		while(token != NULL){
+				tokens[i] = token;
+				token = strtok(NULL, " ");
+				i++;
+		}
+		number_of_tokens = i;
+		//mask to describe the structure of the input
+		//0 for command, 1 for argument, 2 for infile, 3 for outfile
+		//4 for <, 5 for >, 6 for |
+		int mask[number_of_tokens];
+		for(i = 0; i < number_of_tokens; i++){
+			mask[i] = -1;
+		}
+		for(i = 0; i < number_of_tokens; i++){
+			if(i == 0){
+				mask[i] = 0;
+				printf("%s is a command\n", tokens[i]);
+			}else if(strcmp(tokens[i], "|") == 0){
+				mask[i] = 6;
+				printf("%s is a pipe\n", tokens[i]);
+			}else if(mask[i - 1] == 6){
+				mask[i] = 0;
+				printf("%s is a command\n", tokens[i]);
+			}else if(strcmp(tokens[i], "<") == 0){
+				mask[i] = 4;
+				printf("%s is an input redirect\n", tokens[i]);
+			}else if(strcmp(tokens[i], ">") == 0){
+				mask[i] = 5;
+				printf("%s is an output redirect\n", tokens[i]);
+			}
+			else if(mask[i - 1] == 4){
+				mask[i] = 2;
+				printf("%s is an input file\n", tokens[i]);
+			}
+			else if(mask[i - 1] == 5){
+				mask[i] = 3;
+				printf("%s is an output file\n", tokens[i]);
+			}
+			else{
+				mask[i] = 1;
+				printf("%s is an argument\n", tokens[i]);	
+			}
+		}	
+	}
+}

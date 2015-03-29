e2e8b7434ce09387f8174f872bf5a4f7e2a8f232
nathan gibson <gibson_nathaniel1@columbusstate.edu>
2015-03-29 19:48:50 -0400

    main logic in testing

--- simpsh.c
+++ simpsh.c
@@ -11,13 +11,18 @@
 #include <string.h>
 #include <stdio.h>
 #define MAX_LENGTH 256
+
+void exec_single_command(char* command, int pipe[]){
+	if(isspace(command[0])){
+		*command++;
+	}
+	printf("%s", command);
+	
+}
+
 int main(int argc, char *argv[]) {
 	int interactive;
-	char line[MAX_LENGTH];
-	char* tokens[MAX_LENGTH];
-	char* token;
-	int number_of_tokens;
-	int number_of_commands = 1;
+	
 	if(argc == 2){
 		if(strcmp("-i", argv[1]) == 0){
 			interactive = 1;
@@ -30,6 +35,12 @@ int main(int argc, char *argv[]) {
 	}
 	while(1){
 		
+		char line[MAX_LENGTH];
+		char* tokens[MAX_LENGTH];
+		char* token;
+		int number_of_tokens;
+		int number_of_commands = 1;
+		
 		int i = 0;
 		//determine whether or not to print a prompt
 		if(interactive == 1)
@@ -39,9 +50,16 @@ int main(int argc, char *argv[]) {
 			fprintf(stderr, "fgets quiting");
 			return 0;
 		}
+		
 		//get rid of trailing \n
 		line[strlen(line) - 1] = '\0';
-		//tokenize input and store it in an array
+	
+		//make copy of the line
+		char line_copy[strlen(line)];
+		strcpy(line_copy, line);
+		
+		//tokenize the line by spaces and store each
+		//element in an array
 		token = strtok(line, " ");
 		while(token != NULL){
 				tokens[i] = token;
@@ -49,113 +67,33 @@ int main(int argc, char *argv[]) {
 				i++;
 		}
 		number_of_tokens = i;
-		/* 
-		   mask to describe the structure of the input
-		   0 for command, 1 for argument, 2 for infile, 3 for outfile
-		   4 for <, 5 for >, 6 for |, 7 for &
-		*/
-		int mask[number_of_tokens];
-		//initialize mask to known values
-		for(i = 0; i < number_of_tokens; i++){
-			mask[i] = -1;
-		}
-		//determine what each token represents
-		for(i = 0; i < number_of_tokens; i++){
-			if(i == 0){
-				mask[i] = 0;
-			}else if(*tokens[i] == '|'){
-				mask[i] = 6;
-			}else if(*tokens[i] == '&'){
-				mask[i] = 7;
-			}else if(mask[i - 1] == 6){
-				mask[i] = 0;
-			}else if(*tokens[i] == '<'){
-				mask[i] = 4;
-			}else if(*tokens[i] == '>'){
-				mask[i] = 5;
-			}else if(mask[i - 1] == 4){
-				mask[i] = 2;
-			}else if(mask[i - 1] == 5){
-				mask[i] = 3;
-			}else{
-				mask[i] = 1;	
-			}
-		}
+		
 		//find out how many commands are on the line
 		for(i = 0; i < number_of_tokens; i++){
-			if(mask[i] == 6){
+			if(*tokens[i] == '|'){
 				number_of_commands++;
 			}
 		}
-		//store data associated with each command
-		char* cmd;
-		char* args[3] = {NULL, NULL, NULL};
-		char* infile;
-		char* outfile;
-		int output_fd;
-		int input_fd;
+		
+		//tokenize the line based on the
+		//pipe symbol
+		i = 0;
+		char* commands[number_of_commands];
+		token = strtok(line_copy, "|");
+		while(token != NULL){
+				commands[i] = token;
+				token = strtok(NULL, "|");
+				i++;
+		}
+		
+		
 		int tube[2];
-		int fork_rtn, child_status;
-		
-		//parse each element and pull out commands
-		//arguments, etc.
-		for(i = 0; i < number_of_tokens; i++){
-			if(mask[i] == 0){
-				cmd = tokens[i];
-				args[0] = cmd;
-			}
-			else if(mask[i] == 1){
-				args[0] = tokens[i-1];
-				args[1] = tokens[i];
-				args[2] = NULL;
-			}
-			else if(mask[i] == 6){
-				if(number_of_commands > 1){
-					if (pipe(tube)) {
-						fprintf(stderr, "Unable to create pipe.\n");
-						exit(-1);
-					}			
-				}	
-			}
-			else if(mask[i] == 7){
-				
-			}		
+		for(i = 0; i < number_of_commands; i++){
+			exec_single_command(commands[i], tube);	
+			printf("\n");
 		}
 		
-		//handle the case that there is only one command
-		if(number_of_commands == 1){	
-			if (fork_rtn = fork()) {
-				wait(&child_status);
-			}else{	
-				for(i = 0; i < number_of_tokens; i++){
-					if(mask[i] == 2){
-						infile = tokens[i];
-						if ((input_fd = open(infile, O_RDONLY)) >= 0) {
-							close(0);
-							dup2(input_fd, 0);
-						} else {
-							fprintf(stderr, "Unable to open file.\n");
-							exit(1);
-						}
-					}
-					else if(mask[i] == 3){
-						outfile = tokens[i];
-						if ((output_fd = creat(outfile, S_IRUSR | S_IWUSR)) >= 0) {
-							close(1);
-							dup2(output_fd, 1);
-							fchmod(output_fd, S_IRUSR | S_IWUSR | S_IRGRP);
-						} else {
-							fprintf(stderr, "Unable to create file.\n");
-							exit(2);
-						}	
-					}
-				}
-				
-				if(execvp(args[0], args) == -1){
-					fprintf(stderr, "exec error\n");
-				}
-			}
-		}
+		
 	}
 }
 	
